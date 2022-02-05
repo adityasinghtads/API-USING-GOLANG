5d79e3489024cbd7a7e4e365b5a5b1a0bfd1f188
Aditya Singh <thakur.11913421@lpu.in>
2022-02-05 18:05:12 +0530

    6:05 PM

--- database/book.go
+++ database/book.go
@@ -5,6 +5,7 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
+//getBooks is creating connection and interacting from golang app to db server via db variable
 func GetBooks(db *gorm.DB) ([]models.Book, error) {
 
 	books := []models.Book{}
@@ -15,13 +16,36 @@ func GetBooks(db *gorm.DB) ([]models.Book, error) {
 	}
 	return books, nil
 }
-func GetBookByID(db *gorm.DB, id string) (models.Book, error){
-	return models.Book{}, nil
+func GetBookByID(db *gorm.DB, id string) (*models.Book, error) {
+	book := models.Book{}
+	err := db.Select("book.*").Group("books.id").Where("books.id= ?", id).First(&book).Error
+	if err != nil {
+		return nil, err
+	}
+	return &book, nil
 }
-func DeleteBookByID(db *gorm.DB, id string) (error){
+func DeleteBookByID(db *gorm.DB, id string) error {
+	var book models.Book
+	err := db.Where("id=?", id).Delete(&book).Error
+	if err != nil {
+		return err
+	}
 	return nil
 }
 func UpdateBookByID(db *gorm.DB, book *models.Book) error {
+
+	err := db.Save(book).Error // Book is with id
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func SaveBook(db *gorm.DB, book *models.Book) error {
+	err := db.Save(book).Error // book here is without ID
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
+// Add the handler and router..
--- handler/book.go
+++ handler/book.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/adityasinghtads/api_go/database"
+	"github.com/adityasinghtads/api_go/models"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 )
@@ -20,4 +21,19 @@ func (h *Handler) GetBooks(in *gin.Context) {
 		in.JSON(http.StatusInternalServerError, err)
 	}
 	in.JSON(http.StatusOK, books)
+}
+
+func (h *Handler) SaveBook(in *gin.Context) {
+	book := models.Book{}
+	err := in.BindJSON(&book)
+	if err != nil {
+		log.Println(err)
+		in.JSON(http.StatusInternalServerError, err)
+	}
+	err = database.SaveBook(h.DB, &book)
+	if err != nil {
+		log.Println(err)
+		in.JSON(http.StatusInternalServerError, err)
+	}
+	in.JSON(http.StatusOK, book)
 }
